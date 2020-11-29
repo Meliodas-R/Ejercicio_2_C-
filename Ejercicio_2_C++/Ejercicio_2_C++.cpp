30a69320d074348ff36b4a7a29c8ce24979f2e85
Meliodas-R <65538839+Meliodas-R@users.noreply.github.com>
2020-11-29 12:14:06 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio_2_C++/Ejercicio_2_C++.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*
+* 2. Crear distintos tipos de variables y mostrar sus valores por pantalla.
+*
+*/
+int main()
+{
+	int entero = 15;
+	float flotante = 10.45;
+	double doble = 16.3456;
+	char caracter = 'A';
+
+	cout << entero;
+	cout << "\n";
+	cout << flotante;
+	cout << "\n";
+	cout << doble;
+	cout << "\n";
+	cout << caracter;
+	
+
+	return 0;
+}
+
+
